4baf3737db5db74238e87488dedfe95a6922b696
Diego Escudero <dgerod@xyz-lab.org.es>
2014-04-30 15:07:16 +0200

    Created to specific nodes to learn and plan DMPs

---
+++ nodes/learn_dmp_from_trj.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+# ==================================================================================================
+# dgerod.xyz-lab.org.es - 2014
+# --------------------------------------------------------------------------------------------------
+# Learn DMP data from a trajectory.
+# ==================================================================================================
+
+import roslib; roslib.load_manifest('dmp')
+import rospy 
+import numpy as np
+from dmp.srv import *
+from dmp.msg import *
+
+import rosbag
+from dmp.srv._LearnDMPFromDemo import LearnDMPFromDemoResponse
+
+# --------------------------------------------------------------------------------------------------
+
+#Learn a DMP from demonstration data
+def makeLFDRequest(dims, traj, dt, K_gain, 
+                   D_gain, num_bases):
+    demotraj = DMPTraj()
+        
+    for i in range(len(traj)):
+        pt = DMPPoint();
+        pt.positions = traj[i]
+        demotraj.points.append(pt)
+        demotraj.times.append(dt*i)
+            
+    k_gains = [K_gain]*dims
+    d_gains = [D_gain]*dims
+        
+    print "Starting LfD..."
+    rospy.wait_for_service('learn_dmp_from_demo')
+    try:
+        lfd = rospy.ServiceProxy('learn_dmp_from_demo', LearnDMPFromDemo)
+        resp = lfd(demotraj, k_gains, d_gains, num_bases)
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+    print "LfD done"    
+            
+    return resp;
+
+# --------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+  
+    rospy.init_node('learn_dmp_from_trj')
+
+    # Read trajectory from a bag
+    # --------------------------------------------------------
+    
+    # Create a DMP from a 2-D trajectory
+    dims = 2                
+    dt = 1.0                
+    num_bases = 4          
+    traj = [[1.0,1.0],[2.0,2.0],[3.0,4.0],[6.0,8.0]]
+    
+    # Create a DMP from the loaded trajectory
+    # --------------------------------------------------------
+    
+    K = 100                 
+    D = 2.0 * np.sqrt(K)      
+    
+    resp = makeLFDRequest(dims, traj, dt, K, D, num_bases)
+    
+    print resp
+  
+    # Export DMP to another bag
+    # --------------------------------------------------------
+    
+    bag = rosbag.Bag("dmp_data.bag", "w");
+    bag.write("LearnDMPFromDemoResponse",resp)
+    bag.close()
+    
+# ==================================================================================================
--- nodes/plan_trj_using_dmp.py
+++ nodes/plan_trj_using_dmp.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
+
+# ==================================================================================================
+# dgerod.xyz-lab.org.es - 2014
+# --------------------------------------------------------------------------------------------------
+# Plan a trajectory using DMP data.
+# ==================================================================================================
+
 import roslib; 
 roslib.load_manifest('dmp')
 import rospy 
 import numpy as np
 from dmp.srv import *
 from dmp.msg import *
+
+import rosbag
+
+# --------------------------------------------------------------------------------------------------
 
 #Set a DMP as active for planning
 def makeSetActiveRequest(dmp_list):
@@ -29,14 +40,31 @@ def makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh,
             
     return resp;
 
+# --------------------------------------------------------------------------------------------------
+
 if __name__ == '__main__':
   
-    rospy.init_node('dmp_tutorial_node')
+    rospy.init_node('plan_trj_using_dmp')
 
+    # Read bag where DMP data is stored
+    # --------------------------------------------------------
+    
+    bag = rosbag.Bag("dmp_data.bag");
+    for topic, msg, t in bag.read_messages(topics=['LearnDMPFromDemoResponse']):
+      print msg
+    bag.close();    
+  
+    resp = msg;
+        
     # Set it as the active DMP
+    # --------------------------------------------------------
+    
     makeSetActiveRequest(resp.dmp_list)
 
-    # Now, generate a plan
+    # Generate a plan based on DMP
+    # --------------------------------------------------------
+    
+    # Prepare parameters
     x_0 = [0.0,0.0]          # Plan starting at a different point than demo 
     x_dot_0 = [0.0,0.0]   
     t_0 = 0                
@@ -46,7 +74,10 @@ if __name__ == '__main__':
     tau = 2 * resp.tau       # Desired plan should take twice as long as demo
     dt = 1.0
     integrate_iter = 5       # dt is rather large, so this is > 1  
-    plan = makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh, 
-                           seg_length, tau, dt, integrate_iter)
+
+    # Now, generate a plan    
+    plan = makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh, seg_length, tau, dt, integrate_iter)
 
     print plan
+
+# ==================================================================================================
