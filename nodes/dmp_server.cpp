069f895779681d0a5b7e9cddbc198aed1ede5eab
Diego Escudero <dgerod@xyz-lab.org.es>
2015-02-13 22:01:21 +0100

    Renamed python module used to connect to dmp server.

--- nodes/dmp_server.cpp
+++ nodes/dmp_server.cpp
@@ -4,7 +4,7 @@ using namespace dmp;
 std::vector<DMPData> active_dmp_list;
 
 bool lfdCallback(LearnDMPFromDemo::Request  &req,
-			     LearnDMPFromDemo::Response &res )
+			       LearnDMPFromDemo::Response &res )
 {
 	learnFromDemo(req.demo, req.k_gains, req.d_gains, req.num_bases, res.dmp_list);
 	res.tau = req.demo.times[req.demo.times.size()-1];
@@ -12,7 +12,7 @@ bool lfdCallback(LearnDMPFromDemo::Request  &req,
 }
 
 bool planCallback(GetDMPPlan::Request  &req,
-			      GetDMPPlan::Response &res )
+			        GetDMPPlan::Response &res )
 {
 	generatePlan(active_dmp_list, req.x_0, req.x_dot_0, req.t_0, req.goal, req.goal_thresh,
 			     req.seg_length, req.tau, req.dt, req.integrate_iter, res.plan, res.at_goal);
@@ -20,7 +20,7 @@ bool planCallback(GetDMPPlan::Request  &req,
 }
 
 bool activeCallback(SetActiveDMP::Request &req,
-					SetActiveDMP::Response &res )
+					  SetActiveDMP::Response &res )
 {
 	active_dmp_list = req.dmp_list;
 	res.success = true;
@@ -35,6 +35,7 @@ int main(int argc, char **argv)
 	ros::ServiceServer service1 = n.advertiseService("learn_dmp_from_demo", lfdCallback);
 	ros::ServiceServer service2 = n.advertiseService("get_dmp_plan", planCallback);
 	ros::ServiceServer service3 = n.advertiseService("set_active_dmp", activeCallback);
+
 	ROS_INFO("DMP services now ready");
 	ros::spin();
 
